5c2c93936b7cc87304abf547bded8bc8a9e84a0b
luckyaisystems <lucky7series@gmail.com>
2026-05-03 18:43:48 -0400

    Update logic_engine.py

--- logic_engine.py
+++ logic_engine.py
@@ -1,4 +1,4 @@
-def evaluate_rung(rung, inputs):
+def evaluate_rung(rung, inputs, state, dt):
     op = rung["op"]
 
     if op == "AND":
@@ -11,12 +11,34 @@ def evaluate_rung(rung, inputs):
         return {"type": "COIL", "value": not inputs.get(rung["in"][0], False)}
 
     if op == "SET":
-        if all(inputs.get(i, False) for i in rung["in"]):
+        condition = all(inputs.get(i, False) for i in rung["in"])
+        if condition:
             return {"type": "SET", "target": rung["out"]}
         return None
 
     if op == "RESET":
-        if all(inputs.get(i, False) for i in rung["in"]):
+        condition = all(inputs.get(i, False) for i in rung["in"])
+        if condition:
             return {"type": "RESET", "target": rung["out"]}
         return None
+
+    if op == "TON":
+        timer_id = rung["out"]
+        preset = rung["params"]["preset"]
+
+        if timer_id not in state.timers:
+            state.timers[timer_id] = {"acc": 0, "done": False, "preset": preset}
+
+        timer = state.timers[timer_id]
+        condition = all(inputs.get(i, False) for i in rung["in"])
+
+        if condition:
+            timer["acc"] += dt
+            if timer["acc"] >= timer["preset"]:
+                timer["done"] = True
+        else:
+            timer["acc"] = 0
+            timer["done"] = False
+
+        return {"type": "TIMER", "target": timer_id}
 
