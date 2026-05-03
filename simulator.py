4f7465c7083a5f478472cc995cd324828f1df801
luckyaisystems <lucky7series@gmail.com>
2026-05-03 18:44:25 -0400

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -2,12 +2,11 @@ from logic_engine import evaluate_rung
 from plc_state import PLCState
 from rungs import rungs
 
-
-def scan_cycle(state, rungs):
+def scan_cycle(state, rungs, dt):
     next_outputs = state.outputs.copy()
 
     for rung in rungs:
-        result = evaluate_rung(rung, state.inputs)
+        result = evaluate_rung(rung, state.inputs, state, dt)
 
         if result is None:
             continue
@@ -21,6 +20,8 @@ def scan_cycle(state, rungs):
         elif result["type"] == "RESET":
             state.memory[result["target"]] = False
 
+        elif result["type"] == "TIMER":
+            pass  # timer state already updated in state.timers
+
     state.outputs = next_outputs
     return state
-
