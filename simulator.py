6f39b2229db74ade35c0cd9fcf817a252d254a91
luckyaisystems <lucky7series@gmail.com>
2026-05-03 19:11:28 -0400

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -28,20 +28,30 @@ def scan_cycle(state, rungs, dt):
 
     state.outputs = next_outputs
     return state
-
-
 if __name__ == "__main__":
     state = PLCState()
     state.inputs = {"X1": False}
 
     SCAN_TIME = 0.01  # 10 ms
 
+    t = 0  # simple time accumulator
+
     while True:
-        state = scan_cycle(state, rungs, dt=SCAN_TIME * 1000)  # dt in ms
+        # simulate input change at runtime
+        if t == 100:   # after 100 scans (~1 second)
+            state.inputs["X1"] = True
+
+        if t == 200:   # after 200 scans (~2 seconds)
+            state.inputs["X1"] = False
+
+        state = scan_cycle(state, rungs, dt=SCAN_TIME * 1000)
+
         print("Inputs:", state.inputs)
         print("Outputs:", state.outputs)
         print("Timers:", state.timers)
         print("---")
+
         time.sleep(SCAN_TIME)
+        t += 1
 
 
