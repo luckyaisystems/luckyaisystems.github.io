f7faf459447af91b1e3d8da541431c222d6917f2
luckyaisystems <lucky7series@gmail.com>
2026-05-03 18:30:06 -0400

    Update logic_engine.py

--- logic_engine.py
+++ logic_engine.py
@@ -1,25 +1,22 @@
-def evaluate_rung(rung, inputs, state):
+def evaluate_rung(rung, inputs):
     op = rung["op"]
 
-    # Boolean logic
     if op == "AND":
-        return all(inputs.get(i, False) for i in rung["in"])
+        return {"type": "COIL", "value": all(inputs.get(i, False) for i in rung["in"])}
 
     if op == "OR":
-        return any(inputs.get(i, False) for i in rung["in"])
+        return {"type": "COIL", "value": any(inputs.get(i, False) for i in rung["in"])}
 
     if op == "NOT":
-        return not inputs.get(rung["in"][0], False)
+        return {"type": "COIL", "value": not inputs.get(rung["in"][0], False)}
 
-    # Latching logic
     if op == "SET":
         if all(inputs.get(i, False) for i in rung["in"]):
-            state.memory[rung["out"]] = True
-        return state.memory.get(rung["out"], False)
+            return {"type": "SET", "target": rung["out"]}
+        return None
 
     if op == "RESET":
         if all(inputs.get(i, False) for i in rung["in"]):
-            state.memory[rung["out"]] = False
-        return state.memory.get(rung["out"], False)
+            return {"type": "RESET", "target": rung["out"]}
+        return None
 
-    # Timers, counters, etc. will go here later
